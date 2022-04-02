3753278d1219ebd61aa454f5b577e585094db63f
MEzzatM <MEzzatIbrahim0@gmail.com>
2022-04-03 00:41:21 +0200

    edit controller

--- src/application/HelloController.java
+++ src/application/HelloController.java
@@ -634,16 +634,4 @@ public class HelloController implements Initializable {
     }
 
 
-    public void print(double[][] a)
-    {
-    	for(int i=0;i<a.length;i++)
-    	{
-    		for(int j=0;j<a[0].length;j++)
-    		{
-    			System.out.print(a[i][j]+"  ");
-    		}
-    		System.out.print("\n");
-    	}
-    	
-    }
 }
